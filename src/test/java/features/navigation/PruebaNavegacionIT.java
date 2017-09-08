c5c62996b87e6764328183fd471e884ac9a9c6d8
Luis Acuna <luisandresverdugox@gmail.com>
2017-09-08 13:27:42 -0500

    Actualizacion de Selenium Web Driver y Serenity

--- src/test/java/features/navigation/PruebaNavegacionIT.java
+++ src/test/java/features/navigation/PruebaNavegacionIT.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import steps.NavigatinUser;
--- src/test/java/net/serenitybdd/tutorials/ui/GoogleSearchPage.java
+++ src/test/java/net/serenitybdd/tutorials/ui/GoogleSearchPage.java
@@ -3,5 +3,5 @@ package net.serenitybdd.tutorials.ui;
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 
-@DefaultUrl("http://testredcoach.mcets-inc.com/public/websale")
+@DefaultUrl("http://www.ebay.com")
 public class GoogleSearchPage extends PageObject {}
